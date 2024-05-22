f4d3f4a594c0c44616eade870e1b4ab9ef68428e
vijaypoudel <vijay.poudel1@gmail.com>
2024-05-23 00:11:48 +0530

    iterables_custom

---
+++ Iterables/exercise.py
@@ -0,0 +1,22 @@
+class Alphabet:
+    def __init__(self):
+        self.first = 0
+        self.second = 1
+
+    def __iter__(self):
+        return self
+
+
+    def __next__(self):
+        if(self.second >= 100):
+            raise StopIteration
+        else:
+            fib = self.first + self.second
+            self.first = self.second
+            self.second = fib
+            return fib
+
+alpha = Alphabet()
+
+for letter in alpha:
+    print(letter)
