57d2db15d7126ad6b66428864028b89281fb0313
vijaypoudel <vijay.poudel1@gmail.com>
2024-05-26 22:54:15 +0530

    comprehensive list

--- Iterables/generators.py
+++ Iterables/generators.py
@@ -3,3 +3,11 @@ print([2*x for x in range(1,11)])
 print(list(filter((lambda x : x%2 != 0), range (1, 11))))
 
 print([x for x in range (1,11) if x % 2 != 0])
+
+print ([x for x in [ i * 2 for i in range (10)] if x % 8 ==0])
+
+# generate 50 random numbers between 1- 1000 which are multiple of 9
+
+import random
+
+`print([ x for x in [random.randint(1,1001) for i in range(50)] if x % 9 ==0])`
