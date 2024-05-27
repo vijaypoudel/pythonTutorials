82c2dd1bef62af53301ea005e3baf71e6aea1bb1
vijaypoudel <vijay.poudel1@gmail.com>
2024-05-27 22:11:24 +0530

    Generator

--- Iterables/generators.py
+++ Iterables/generators.py
@@ -10,4 +10,4 @@ print ([x for x in [ i * 2 for i in range (10)] if x % 8 ==0])
 
 import random
 
-`print([ x for x in [random.randint(1,1001) for i in range(50)] if x % 9 ==0])`
+print([ x for x in random.sample(range(1,1001),50) if x % 9 ==0])
