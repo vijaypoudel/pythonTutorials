9796cee15915dc618e695549554d152b246883aa
vijaypoudel <vijay.poudel1@gmail.com>
2024-05-30 23:12:35 +0530

    dictionary_practice

--- Dictionary/practice_1.py
+++ Dictionary/practice_1.py
@@ -12,6 +12,18 @@
 #     print(derek_dictionary)
 
 
+# list_of_customer = []
+# while True:
+#     response = str(input("Enter Customer (Yes/No)")).lower().rstrip()
+#     if response == "yes":
+#         first_name, last_name = str(input("Enter Customer Name")).split()
+#         list_of_customer.append({"f_name" : first_name , "l_name" : last_name})
+#         continue
+#     else:
+#         break
+# for i in list_of_customer:
+#     print(i['f_name'] , i['l_name'])
+
 list_of_customer = []
 while True:
     response = str(input("Enter Customer (Yes/No)")).lower().rstrip()
